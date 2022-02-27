c0b798542be443aeb6cfd6630bb9b8af0037fcf4
hbkdshamal <64688853+dilukshashamal@users.noreply.github.com>
2022-02-27 19:25:19 +0530

    Delete src directory

--- src/main/java/com/example/versions/model/AndroidVersion.kt
+++
@@ -1,6 +0,0 @@
-package com.example.versions.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class AndroidVersion(@StringRes val stringResourceID : Int, @DrawableRes val imageResourceID : Int)
