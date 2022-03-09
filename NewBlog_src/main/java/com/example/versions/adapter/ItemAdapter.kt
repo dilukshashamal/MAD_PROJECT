a25da0f752760bb4583736c6032e8ecd151966f0
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2022-03-09 18:06:03 +0700

    UI change in blog page

---
+++ NewBlog_src/main/java/com/example/versions/adapter/ItemAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.versions.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.versions.R
+import com.example.versions.model.AndroidVersion
+
+class ItemAdapter(private val context: Context, private val dataset: List<AndroidVersion>)
+    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+        class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+            val textView: TextView = view.findViewById(R.id.item_title)
+            val imageView : ImageView = view.findViewById(R.id.item_image)
+
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        //TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceID)
+        holder.imageView.setImageResource(item.imageResourceID)
+
+    }
+
+    override fun getItemCount(): Int {
+        //TODO("Not yet implemented")
+        return dataset.size
+    }
+
+
+
+}
+
